9ff9078bf6748b130a6cd0e993a7f04ba273d3e8
victor <mcuitlahuac62@gmail.com>
2022-09-11 01:19:59 -0400

    Repo Game wuit Python

---
+++ diccionary.py
@@ -0,0 +1,19 @@
+from calendar import month
+
+
+monthConversions = {
+    "Jan": "January",
+    "Feb": "February",
+    "Mar": "March",
+    "Apr": "April",
+    "May": "May",
+    "Jun": "Jun",
+    "Jul": "July",
+    "Aug": "August",
+    "Sep": "September",
+    "Oct": "October",
+    "Nov": "November",
+    "Dic": "December"
+}
+
+print(monthConversions ["Nov"])
